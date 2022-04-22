b31d19d5dc2b2bc4b82f5b4ca86dd7099516fe89
Andre Blaze <kuraiokamiblaze@gmail.com>
2022-04-22 13:22:27 -0600

    project - router/model: GET Path set up

--- api/project/model.js
+++ api/project/model.js
@@ -1,2 +1,36 @@
 // build your `Project` model here
-const db
+const db = require('../../data/dbConfig')
+
+async function getProjects(){
+    const projectsResults = await db('projects')
+
+    if(projectsResults.length === 0){
+        return []
+    }
+
+    const projects = []
+
+    for(let project of projectsResults){
+        const proj = project
+        projects.push({
+            project_id: proj.project_id,
+            project_name: proj.project_name,
+            project_description: proj.project_description,
+            project_completed: 
+                proj.project_completed === 0 
+                    ? false : proj.project_completed
+        })
+    }
+
+    return projects
+}
+
+async function createProject(project){
+    const [project_id] = await db('projects').insert(project)
+
+}
+
+module.exports = {
+    getProjects,
+    createProject
+} 
--- api/project/router.js
+++ api/project/router.js
@@ -1,13 +1,18 @@
 // build your `/api/projects` router here
 const express = require('express')
-// const Projects = require('./model')
+const Projects = require('./model')
 
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    // Example of response body: 
-    // [{ "project_id":1, "project_name":"bar", "project_description":null, "project_completed":false }]
-    res.send('Get Projects')
+    Projects.getProjects()
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({message: 'Could not get projects'})
+        })
 })
 
 router.post('/', (req, res, next) => {
